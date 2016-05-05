88b51853abb2edf9de455c0e0548bcaec6429576
LorenziStefano <LorenziStefano@AM-SLORENZI.itmaster.local>
2016-05-05 23:00:04 +0200

    all test

---
+++ it.design.pattern/src/test/java/it/design/pattern/AllTests.java
@@ -0,0 +1,10 @@
+package it.design.pattern;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ChainOfResponsibilityTests.class, ServiceLocatorTests.class, ServiceLocatorAndChainTests.class })
+public class AllTests {
+	// do nothing
+}
